403ba9907b31cf970f3df3c25bf44fab31b95a87
CreativeCodeCat <wayne6324@gmail.com>
2026-01-06 12:06:14 +0000

    Explicitly configure metadata sources for Maven Central
    This commit updates the `settings.gradle.kts` file to explicitly configure the metadata sources for the `mavenCentral()` repository. The `mavenPom()` and `artifact()` sources are now specified. This change can help resolve dependency resolution issues by ensuring consistent and reliable metadata retrieval.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,8 +16,13 @@ dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
-        mavenCentral()
-        maven(url = "https://jitpack.io")
+        mavenCentral {
+            metadataSources {
+                mavenPom()
+                artifact()
+            }
+        }
+        maven("https://jitpack.io")
     }
 }
 
